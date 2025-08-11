b03945b77b9d968d6f061708af6b7f202ca3b850
kafeelkazeem <kafeelkazeem@gmail.com>
2025-08-11 14:38:44 +0100

    update: completed send message controller

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -13,7 +13,23 @@ interface ISendMessageRequest {
 
 export const sendMessage = async (req: Request, res: Response, io: SocketIOServer) =>{
     try {
-        
+        const { chatRoomId, senderId, content, type } = req.body as ISendMessageRequest
+
+        if (!chatRoomId || !senderId || !content) {
+            return res.status(400).json({ error: 'Missing required fields: chatRoomId, senderId, content.' });
+        }
+
+        const newMessage = new Message({chatRoomId, senderId, content, type});
+        await newMessage.save()
+
+        await ChatRoom.findByIdAndUpdate(chatRoomId, {
+            $set: { lastMessage: newMessage._id },
+        });
+
+        io.to(chatRoomId).emit('newMessage', newMessage);
+
+        res.status(201).json({ success: true, message: 'Message sent successfully.', data: newMessage });
+
     } catch (error) {
         console.log(error)
     }
